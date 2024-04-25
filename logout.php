56f5c0b641010648957648112c3fedbf2767fe74
MuhammadAbas <122088412+hyzo70@users.noreply.github.com>
2024-04-26 00:14:39 +0800

    Add files via upload

--- logout.php
+++ logout.php
@@ -8,5 +8,9 @@ if(isset($_SESSION['user_id']))
 
 }
 
+session_unset(); // remove all stored values in session variables
+session_destroy(); // Destroys all data registered to a session
+session_write_close(); // End the current session and store session data.
+setcookie(session_name(),'',0,'/'); // Send Cookie to client web browser.
 header("Location: login.php");
 die;
